bf7b8a1986a550ca4fda05a02735038501f7c9ec
tharindu <chathurangewdt@gmail.com>
2025-03-02 11:47:45 +0100

    git diff keymap

--- lua/plugins/git-integration.lua
+++ lua/plugins/git-integration.lua
@@ -9,6 +9,7 @@ return {
       
       vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
       vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+      vim.keymap.set("n", "<leader>gc", ":Gitsigns diffthis HEAD~1<CR>", {})
     end
   }
 }
